c1b4d946e0941b0b79eddb9e0a14e5248dd896a9
Rafael-Suzin <142227909+Rafael-Suzin@users.noreply.github.com>
2023-11-09 00:58:32 -0300

    Add files via upload

--- System solver/src/System solver.cpp
+++ System solver/src/System solver.cpp
@@ -1,50 +1,99 @@
-//v0.001
+//v0.003
 // Created on: Sep 26, 2023
 //     Author: Rafael
 #include <iostream>
-#include <cmath>
 #include <vector>
-#include <utility>
+#include <string.h>
 
 using namespace std;
 
 class System_Solver {
 
+    vector<char> var;
+    vector<int> cof; //com erro era para ser uma matriz
+//   vector<vector<int>> matriz(linhas,vector<int>(colunas));
+    vector<int> igual;
+    vector<int> value;
 
-    char var;
-    int cof;
-    int igual;
-    int value;
+    int n_var;
+    int n_eq;
 
     void incio(){
     	introduction();
     	dados();
     	imprime_exp();
-    	solve();
-    	imprime_val();
+    	system_analize();
+//    	solve();
+//    	imprime_val();
 
     }
     void introduction(){
-    	cout <<"system solver 0.0001v \n";
-    	cout <<"sistema linear de uma variavel\n";
+    	cout <<"system solver 0.003v \n";
+    	cout <<"sistema linear simples\n";
     }
     void dados(){
-    	cout <<"insira o nome da sua primeria variavel\n";
-    	cin >> var;
-    	cout <<"insira o coefiente da variavel\n";
-    	cin >> cof;
-    	cout <<"insira a igualdade\n";
-    	cin >> igual;
+    	cout <<"insira quantas variaveis e quantas equações\n";
+    	cin >>n_var>>n_eq;
+    	resize();
+
+		for (int i = 0; i < n_var; i++) {
+			cout <<"insira o nome da sua " <<(i+1)<<"º variavel\n";
+			cin >> var[i];
+		}
+    	for (int i = 0;  i< n_eq; i++) {
+			for (int j = 0; j < n_var; ++j) {
+
+				cout <<"insira o coefiente da variavel "<<var[j]<<"\n";
+				cin >> cof[j];
+			}
+			cout <<"insira a igualdade\n";
+			cin >> igual[i];
+		}
     }
     void imprime_exp(){
-    	cout <<cof<<var<<"="<<igual<<"\n";
+		for (int i = 0;  i< n_eq; i++) {
+			for (int j = 0; j < n_var; ++j) {
+				cout <<cof[j]<<var[j];
+				if(i+1<n_eq){
+					cout<<"+";
+				}
+			}
+			cout<<"="<<igual[i]<<"\n";
+		}
     }
     void solve(){
-    	value=igual/cof;
+    	value[0]=igual[0]/cof[0];
+    	imprime_val();
     }
     void imprime_val(){
     	cout <<"sistema resolvido:\n";
-    	cout <<var<<"="<<value<<"\n";
+    	cout <<var[0]<<"="<<value[0]<<"\n";
+    }
+    void resize(){
+    	var.resize(n_var);
+		cof.resize(n_var);
+		igual.resize(n_eq);
+		value.resize(n_var);
+    }
+    void system_analize(){
+
+    	if (n_var==(n_eq==1)){
+			cout <<"sistema é simples\n";
+			solve();
+
+    	}
+    	else if(n_var==n_eq){ //det!=0
+			cout <<"o sistema é posivel e determinado\n";
+			//solve_cramer
+    	}
+    	else if(n_var>1&&n_eq==1){
+			cout <<"o sistema possui variaveis livres, mas já está minimizado em apenas uma equação\n";
+//    		Solver_mat.isolar(var[1]);
+		}
+    	else { //(en_eq)
+			cout <<"o sistema pode ou não ser possivel, requer simplificação\n";
+//    		Solver_mat.simplificar?
+		}
     }
 public:
     System_Solver(){
@@ -54,6 +103,6 @@ public:
 
 int main() {
 	System_Solver();
-
     return 0;
 }
+
