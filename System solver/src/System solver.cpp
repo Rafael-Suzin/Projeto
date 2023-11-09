b356789df0d6f989a6bc0faa569e1cea6221eb67
Rafael-Suzin <142227909+Rafael-Suzin@users.noreply.github.com>
2023-11-09 02:36:12 -0300

    Add files via upload

--- System solver/src/System solver.cpp
+++ System solver/src/System solver.cpp
@@ -1,181 +1,117 @@
-//v0.005
 #include <iostream>
+#include <Eigen/Dense>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-class Matriz {
+class SystemSolver {
 public:
-    vector<vector<int>> matriz;
-    int det=0;
-    bool det_is_set=false;
-
-    Matriz() {
-    	det=0;
-    }
-    Matriz(vector<vector<int>> m) {
-    	resize_m(m.size(),m[0].size());
-		matriz = m;
-		determinante();
-    }
-    Matriz(int linhas, int colunas) {
-    	resize_m(linhas,colunas);
-    	preencherMatriz();
-		determinante();
+    SystemSolver() {
+        introduction();
+        input();
+        printEquations();
+//        system_analize(); // precisa ser refeito, pois para sistema imposiveis retorna respostas, para aqueles com mais de uma resposta retorna uma errada
+        solve();
+        printSolution();
     }
 
-    void preencherMatriz() {
-        for (unsigned int i = 0; i < matriz.size(); i++) {
-            for (unsigned int j = 0; j < matriz[i].size(); j++) {
-                cout << "Digite o valor para a posição [" << i << "][" << j << "]: ";
-                cin >> matriz[i][j];
-            }
+    SystemSolver(Eigen::MatrixXd& cof,Eigen::VectorXd& igual) {
+        this->cof = cof;
+        this->igual = igual;
+        n_var = cof.cols();
+        n_eq = cof.rows();
+        var.resize(n_var, " ");
+//        char a = 'a'; //'a'=97
+        for (int i = 97; (i-97) < n_var; i++) {
+            var[i-97] = std::string(1, i);
         }
+        introduction();
+        printEquations();
+        solve();
+        printSolution();
     }
-    void imprimirMatriz() {
-        for (unsigned int i = 0; i < matriz.size(); i++) {
-            for (unsigned int j = 0; j < matriz[i].size(); j++) {
-                cout << matriz[i][j] << " ";
-            }
-            cout << endl;
-        }
+
+    void solve() {
+        solution = cof.colPivHouseholderQr().solve(igual);
     }
-	int size(){
-		return sizeof(matriz)/sizeof(matriz[0][0]);
-	}
-	void resize_m(int lin,int col){
-		matriz.resize(lin, vector<int>(col));
-	}
-	int determinante(){
-		if (matriz.size() == 2 && matriz[0].size() == 2){
-			det=(matriz[0][0]*matriz[1][1]-matriz[0][1]*matriz[1][0]);
-			det_is_set=true;
-		}
-		if(matriz.size() == 1 && matriz[0].size() == 1){
-			det=(matriz[0][0]);
-			det_is_set=true;
-		}
-		return det;
-	}
-	bool det_set(){return det_is_set;};
-	int get_det(){
-		if(det_is_set){
-			return det;
-		}
-		cout<<"det not set";
-		return 0;
-	}
-    vector<vector<int>> getMatriz() {
-        return matriz;
+
+    Eigen::VectorXd getSolution() {
+        return solution;
     }
-};
 
-class System_Solver {
-
-    vector<char> var;
-    Matriz cof;
-    vector<int> igual;
-    vector<int> value;
-
+private:
+    Eigen::MatrixXd cof; //matriz nxn// ainda não testei como fica matrizes não quadradas nisso
+    Eigen::VectorXd igual;
+    Eigen::VectorXd solution;
 
     int n_var;
     int n_eq;
+    vector<string> var;
 
-    void incio(){
-    	introduction();
-    	dados();
-    	imprime_exp();
-    	system_analize();
-//    	solve();
-//    	imprime_val();
+    void introduction() {
+        cout << "System Solver 0.1\n";
+        cout << "Resolvendo um sistema linear simples\n";
+    }
 
-    }
-    void introduction(){
-    	cout <<"system solver 0.005v \n";
-    	cout <<"sistema linear simples\n";
-    }
-    void dados(){
-    	cout <<"insira quantas variaveis e quantas equaçoes \n";
-    	cin >>n_var>>n_eq;
-    	resize();
+    void input() {
+        cout << "Insira o número de variáveis e equações: ";
+        cin >> n_var >> n_eq;
 
-		for (int i = 0; i < n_var; i++) {
-			cout <<"insira o nome da sua " <<(i+1)<<"º variavel\n";
-			cin >> var[i];
-		}
-    	for (int i = 0;  i< n_eq; i++) {
-			for (int j = 0; j < n_var; ++j) {
+        cof.resize(n_eq, n_var);
+        igual.resize(n_eq);
+        var.resize(n_var);
 
-				cout <<"insira o coefiente da variavel "<<var[j]<<"\n";
-				cin >> cof.matriz[i][j];
-			}
-			cout <<"insira a igualdade\n";
-			cin >> igual[i];
-		}
-//    	cout <<cof.size();
-    }
-    void imprime_exp(){
-		for (int i = 0;  i< n_eq; i++) {
-			for (int j = 0; j < n_var; ++j) {
-				cout <<cof.matriz[i][j]<<var[j];
-				if(j+1<n_eq){
-					cout<<"+";
-				}
-			}
-			cout<<"="<<igual[i]<<"\n";
-		}
-    }
-    void solve(){
-//    	value[0]=igual[0]/cof[0];
-    	imprime_val();
-    }
-    void imprime_val(){
-    	cout <<"sistema resolvido:\n";
-    	cout <<var[0]<<"="<<value[0]<<"\n";
-    }
-    void resize(){
-//    	cout<<"aumentando o vetor das variaveis \n";
-    	var.resize(n_var);
-//    	cout<<"aumentando a matriz de coficientes \n";
-    	cof.resize_m(n_eq,n_var);
-		igual.resize(n_eq);
-		value.resize(n_var);
-    }
-    void system_analize(){
-    	cof.determinante();
-    	if (n_var<=2){
-			cout <<"determinate ="<<cof.det<<"\n";
-    	}
-    	if (n_var==(n_eq==1)){
-			cout <<"sistema simples\n";
-			solve();
+        for (int i = 0; i < n_var; i++) {
+        	cout <<"insira o nome da " <<(i+1)<<"º variavel:\n";
+            cin >> var[i];
+        }
 
-    	}
-    	else if(n_var==n_eq&&cof.det!=0){ //det!=0
-			cout <<"o sistema posivel e determinado\n";
-			//solve_cramer ou matriz inversa
-    	}
-    	else if(n_var>1&&n_eq==1){
-			cout <<"o sistema possui variaveis livres, mas já está minimizado em apenas uma equação\n";
-//    		Solver_mat.isolar(var[1]);
-		}
-    	else { //(en_eq)
-			cout <<"o sistema pode ou não ser possivel, requer simplificação\n";
-//    		Solver_mat.simplificar?
-		}
+        for (int i = 0; i < n_eq; i++) {
+            for (int j = 0; j < n_var; j++) {
+                cout << "Insira o coeficiente da variável " << var[j] << " na equação " << (i + 1) << "º: ";
+                cin >> cof(i, j);
+            }
+            cout << "Insira o valor da igualdade na equação " << (i+1) << ": ";
+            cin >> igual(i);
+        }
     }
-public:
-    System_Solver(){
-    	incio();
+
+    void printEquations() {
+        cout << "Sistema de Equações:\n";
+        for (int i = 0; i < n_eq; i++) {
+            cout << "Equação " << (i + 1) << ": ";
+            for (int j = 0; j < n_var; j++) {
+                cout << cof(i, j) << var[j] << (j == n_var - 1 ? "" : " + ");
+            }
+            cout << " = " << igual(i) << "\n";
+        }
+    }
+
+    void printSolution() {
+        cout << "Solução do sistema:\n";
+        for (int i = 0; i < n_var; i++) {
+            cout << "Variável " << var[i] << " = " << solution(i) << "\n";
+        }
     }
 };
 
 int main() {
+    // Caso 1: Chame SystemSolver com entrada do usuário
+//    SystemSolver system1;
 
+//     Caso 2: Chame SystemSolver com matriz de coeficientes e vetor de constantes
+    Eigen::MatrixXd cof(2, 2);
+    Eigen::VectorXd igual(2);
+    cof << 2, 3, 6, 9;
+    igual << 5, 15;   //sistema posivel e indeterminado, solução geral = (x,(5-2x)/3), o programa retorna uma resposta errada
 
-	System_Solver();
+    SystemSolver system2(cof, igual);
+    Eigen::VectorXd solution2 = system2.getSolution();
+
+    cout << "Solução do sistema:\n";
+    cout << "x = " << solution2(0) << "\n";
+    cout << "y = " << solution2(1) << "\n";
 
     return 0;
 }
-
-
